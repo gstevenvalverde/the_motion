bb767d4572e98cd1c09aadd7c4ecd4d2e92f9354
gstevenvalverde <63868604+gstevenvalverde@users.noreply.github.com>
2022-11-05 09:59:27 -0400

    Update Game.py

--- Game.py
+++ Game.py
@@ -1,17 +1,7 @@
 import pygame
 import Boy
-import CommandUpdate
-import CommandDraw
 
-import CommandUp
-import CommandDown
-import CommandLeft
-import CommandRight
-import CommandUpLeft
-import CommandUpRight
-import CommandDownLeft
-import CommandDownRight
-import CommandStop
+import Invoker
 
 
 class Game:
@@ -21,21 +11,7 @@ class Game:
 
     def __init__(self):
         self.player = Boy.Boy()
-        self.CommandUpdate = CommandUpdate.CommandUpdate(self.player)
-        self.CommandDraw = CommandDraw.CommandDraw(self.player, self.SCREEN)
-
-        self.CommandUp = CommandUp.CommandUp(self.player)
-        self.CommandDown = CommandDown.CommandDown(self.player)
-        self.CommandLeft = CommandLeft.CommandLeft(self.player)
-        self.CommandRight = CommandRight.CommandRight(self.player)
-        self.CommandDownLeft = CommandDownLeft.CommandDownLeft(self.player)
-        self.CommandDownRight = CommandDownRight.CommandDownRight(self.player)
-        self.CommandUpLeft = CommandUpLeft.CommandUpLeft(self.player)
-        self.CommandUpRight = CommandUpRight.CommandUpRight(self.player)
-        self.CommandStop = CommandStop.CommandStop(self.player)
-
-        # self.invoker_update = Invoker.Invoker(CommandUpdate)
-        # self.invoker_draw = Invoker.Invoker(CommandDraw)
+        self.invoker = Invoker.Invoker(self.player, self.SCREEN)
 
     def main(self):
         pygame.init()
@@ -45,32 +21,30 @@ class Game:
         while run:
 
             for event in pygame.event.get():
-                self.CommandUpdate.execute()
+                self.invoker.action(8)
                 if event.type == pygame.QUIT:
                     run = False
                 user_input = pygame.key.get_pressed()
                 if user_input[pygame.K_w]:
-                    self.CommandUp.execute()
+                    self.invoker.action(0)
                 elif user_input[pygame.K_s]:
-                    self.CommandDown.execute()
+                    self.invoker.action(1)
                 elif user_input[pygame.K_a]:
-                    self.CommandLeft.execute()
+                    self.invoker.action(2)
                 elif user_input[pygame.K_d]:
-                    self.CommandRight.execute()
+                    self.invoker.action(3)
                 elif user_input[pygame.K_q]:
-                    self.CommandUpLeft.execute()
+                    self.invoker.action(4)
                 elif user_input[pygame.K_e]:
-                    self.CommandUpRight.execute()
+                    self.invoker.action(5)
                 elif user_input[pygame.K_z]:
-                    self.CommandDownLeft.execute()
+                    self.invoker.action(6)
                 elif user_input[pygame.K_c]:
-                    self.CommandDownRight.execute()
-                elif user_input[pygame.K_SPACE]:
-                    self.CommandStop.execute()
+                    self.invoker.action(7)
 
             self.SCREEN.fill((255, 255, 255))
 
-            self.CommandDraw.execute()
+            self.invoker.action(9)
 
             clock.tick(30)
             pygame.display.update()
